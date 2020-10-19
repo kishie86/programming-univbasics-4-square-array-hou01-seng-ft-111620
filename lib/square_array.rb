63d280b3abeb029d319cd90c4ea03daf687c7394
Kishana Liburd <kishana.liburd@gmail.com>
2020-10-19 02:33:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
   count = 0
   while count < array.length do
- new_array.push(array[count].to_s)
+ new_array.push(array[count]Math.sqrt)
   count += 1 
 end
 new_array
